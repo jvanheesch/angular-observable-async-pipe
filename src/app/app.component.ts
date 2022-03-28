c9851d3aaaa97369b67591ab073a81280539c5dd
jvanheesch <joris.vanheesch@vdab.be>
2022-03-28 11:31:38 +0200

    naive implemenation - 'manual' subscribtion in OnInit - 'glitch' when rendering formeleOpvolgingStatus == true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormeleOpvolgingService} from "./formele-opvolging.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public formeleOpvolgingStatus: boolean | null = null;
 
+  constructor(private formeleOpvolgingService: FormeleOpvolgingService) {
+  }
+
+  ngOnInit(): void {
+    this.formeleOpvolgingService.getFormeleOpvolging()
+      .subscribe(s => this.formeleOpvolgingStatus = s.status);
+  }
+
   toggleFormeleOpvolging(): void {
-    this.formeleOpvolgingStatus = !this.formeleOpvolgingStatus;
+    let nieuweStatus = !this.formeleOpvolgingStatus;
+    this.formeleOpvolgingService.wijzigFormeleOpvolging(nieuweStatus)
+      .subscribe();
   }
 }
--- src/app/formele-opvolging.service.ts
+++ src/app/formele-opvolging.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {delay, map, Observable, of, ReplaySubject} from "rxjs";
+import {FormeleOpvolging} from "./formele-opvolging";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormeleOpvolgingService {
+  private formeleOpvolgingSubject: ReplaySubject<FormeleOpvolging>;
 
-  constructor() { }
+  constructor() {
+    // replace with http GET
+    let o = of({status: true})
+      .pipe(delay(1000));
+    this.formeleOpvolgingSubject = this.cacheable(o);
+  }
+
+  public getFormeleOpvolging(): Observable<FormeleOpvolging> {
+    return this.formeleOpvolgingSubject;
+  }
+
+  public wijzigFormeleOpvolging(status: boolean): Observable<void> {
+    // replace with http PUT
+    let o = of(null)
+      .pipe(delay(1000));
+    return o
+      .pipe(map(status200 => this.formeleOpvolgingSubject!.next({status: status})))
+  }
+
+  private cacheable<T>(o: Observable<T>): ReplaySubject<T> {
+    const replay = new ReplaySubject<T>(1);
+    o.subscribe(
+      x => replay.next(x),
+      x => replay.error(x),
+      // () => replay.complete()
+    );
+    return replay;
+  }
 }
